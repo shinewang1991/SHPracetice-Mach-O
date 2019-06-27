8cd70b014efa4eabc4cfafd83e70a260a9e0343f
Eirc <eric.wang@qiffa.com>
2019-06-27 14:28:17 +0800

    Initial Commit

---
+++ SHPractice-Mach-O/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SHPractice-Mach-O
+//
+//  Created by Eric on 2019/6/27.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
